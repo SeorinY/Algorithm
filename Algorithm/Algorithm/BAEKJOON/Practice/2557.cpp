cc23afe7921408de04af80825b5ca2fb0a9c5f55
유서린 <sprout3082@gmail.com>
2023-01-16 16:43:43 +0900

    [Solved] - 10828

--- Algorithm/Algorithm/BAEKJOON/Practice/2557.cpp
+++ Algorithm/Algorithm/BAEKJOON/Practice/2557.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main(void){
-    cout<<"Hello World!";
+    int n;
+    cin>>n;
+    string str;
+    int num = 0;
+    vector<int> v;
+    for(int i=0; i<n;i++){
+        cin >> str;
+        if (str== "push"){
+            cin >> num;
+            v.push_back(num);
+        }else if (str == "pop"){
+            if(v.empty())
+                cout << -1<<"\n";
+            else {
+                cout << v.back() << "\n";
+                v.pop_back();
+            }
+        }else if (str=="size") {
+            cout<< v.size() << "\n";
+        }else if (str == "empty") {
+            if (v.empty())
+                cout<< 1 <<"\n";
+            else
+                cout<< 0 << "\n";
+        }else if (str == "top") {
+            if (v.empty())
+                cout<<-1<<"\n";
+            else
+                cout<<v.back()<<"\n";
+        }
+    }
+    
 }
+
